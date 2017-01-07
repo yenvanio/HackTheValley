36d8517351f050c2cfb8012c58bfa0be8741fffa
Dongoug <dougouk@gmail.com>
2017-01-07 13:57:41 -0500

    Updating view

--- app/src/main/java/me/mathusan/parkthevalley/Map/MapFragment.java
+++ app/src/main/java/me/mathusan/parkthevalley/Map/MapFragment.java
@@ -1,8 +1,11 @@
 package me.mathusan.parkthevalley.Map;
 
+import android.app.Fragment;
+
 /**
  * Created by Dan on 07/01/2017.
  */
 
-public class MapFragment {
+public class MapFragment extends Fragment {
+
 }

19ab6ed8dab6e2c0c7b6f673e20aa47447553c9c
Mathusan Selvarajah <mathusans52@gmail.com>
2017-01-07 19:24:35 -0500

    Gradle update

--- app/src/main/java/me/mathusan/parkthevalley/MainActivity.java
+++ app/src/main/java/me/mathusan/parkthevalley/MainActivity.java
@@ -76,6 +76,9 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        
+
+
     }
 
     @Override
